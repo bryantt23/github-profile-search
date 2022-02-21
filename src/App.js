64a412ef3bbf768ed65fc206a23d5b3177580eef
bryant tunbutr <bryantt23@users.noreply.github.com>
2022-02-21 13:28:07 -0800

    add axios, page states

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+
+const pageStates = { input: 'input', output: 'output', loading: 'loading' };
 
 function App() {
+  const [profile, setProfile] = useState('bryantt23');
+  const [pageState, setPageState] = useState(pageStates.input);
+
+  const getMyProfile = async profile => {
+    try {
+      const response = await axios.get(
+        `https://api.github.com/users/${profile}`
+      );
+      return response.data;
+    } catch (e) {
+      throw e;
+    }
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    alert(`The name you entered was: ${profile}`);
+    setPageState(pageStates.loading);
+    const res = await getMyProfile(profile);
+    console.log('🚀 ~ file: App.js ~ line 21 ~ handleSubmit ~ res', res);
+    setPageState(pageStates.output);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {JSON.stringify(pageState)}
+      {pageState === pageStates.input ? (
+        <div>
+          <p>{profile}</p>
+
+          <form onSubmit={handleSubmit}>
+            <label>
+              Enter your name:
+              <input
+                type='text'
+                onChange={e => setProfile(e.target.value)}
+                value={profile}
+              />
+            </label>
+            <input type='submit' />
+          </form>
+        </div>
+      ) : pageState === pageStates.loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 }
